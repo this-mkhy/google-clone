256eb420d369bce8cc4574e35fb87578b5c1b144
mkhy19 <mkhy88519@gmail.com>
2022-03-25 06:12:58 +0200

    fix loading spinner issue

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,19 @@
 import React from 'react';
 //import { Loader } from 'react-loader-spinner';
-// Rings, TailSpin and etc
-import * as Loader from "react-loader-spinner";
 
+// Rings, TailSpin and etc
+//import * as Loader from "react-loader-spinner";
+import {Puff} from 'react-loader-spinner';
+ 
 export const Loading = () => (
   <div className="flex justify-center items-center ">
-    <Loader.TailSpin type="Puff" color="#00BFFF" height={550} width={80} />
+    {/* <Loader.TailSpin type="TailSpin" color="#00BFFF" height={550} width={80} /> */}
+    {/* <Loader.RotatingLines
+      width="100"
+      strokeColor="#6495ED"
+      strokeWidth="1"
+      animationDuration="3"
+    /> */}
+    <Puff color="#00BFFF" height={550} width={80} />
   </div>
 );
